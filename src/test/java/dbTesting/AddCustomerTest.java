181b3d133bbd5d8614c895d47648be6e6a6505a4
rbkaw <rbkawale7@gmail.com>
2026-07-30 08:02:43 +0530

    added addcustomer test medthod

--- src/test/java/dbTesting/AddCustomerTest.java
+++ src/test/java/dbTesting/AddCustomerTest.java
@@ -1,4 +1,13 @@
 package dbTesting;
 
+import org.testng.annotations.Test;
+
 public class AddCustomerTest {
+
+    @Test
+    public void addCustomertest()
+    {
+        System.out.println("Add customer");
+    }
+
 }
